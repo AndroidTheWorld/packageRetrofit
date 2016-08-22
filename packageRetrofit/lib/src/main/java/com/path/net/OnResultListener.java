21780ba6a0ffef5e71ac5ac6c94ab1f79b38a2cd
jinsea <549024741@qq.com>
2016-08-22 16:54:12 +0800

    提高方法的识别度

--- packageRetrofit/lib/src/main/java/com/path/net/OnResultListener.java
+++ packageRetrofit/lib/src/main/java/com/path/net/OnResultListener.java
@@ -10,12 +10,12 @@ public class OnResultListener<T> {
     public boolean onCache(String cache) {//返回true,表示不再请求网络
         return false;
     }
-    public boolean onCache(T cache) {//返回true,表示不再请求网络
+    public boolean onCache(T object) {//返回true,表示不再请求网络
         return false;
     }
 
     public  void onSuccess(String result){}
-    public  void onSuccess(T result){}
+    public  void onSuccess(T object){}
 
     public void onFailure(String message) {//根据需求实现
     }
@@ -23,7 +23,7 @@ public class OnResultListener<T> {
     public void onFailure(String cache, String message) {//根据需求实现，当请求异常时，需要使用缓存时，调用此方法
     }
 
-    public void onFailure(T cache, String message) {//根据需求实现，当请求异常时，需要使用缓存时，调用此方法
+    public void onFailure(T object, String message) {//根据需求实现，当请求异常时，需要使用缓存时，调用此方法
     }
 
     @Deprecated
